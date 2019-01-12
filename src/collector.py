8527b7fd42733b476b8f250c8f28c5745efa03e0
mvfurer <mvfurer@gmail.com>
2019-01-11 21:38:15 -0300

    adding handle signal INT

--- src/collector.py
+++ src/collector.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python
 
 from scapy.all import *
-
+import os
 import sys
+import signal
 
 from collectorClass import Collector
 
 programName = "collector"
+global shutdown_flag
+shutdown_flag = 0
+
+
+def terminate_process(signal_number, frame):
+    global shutdown_flag
+    shutdown_flag = 1
+    print('(SIGTERM) terminating the process')
 
 
 def main():
-    print("starting " + programName + " main ...")
+    signal.signal(signal.SIGINT, terminate_process)
+    print("starting " + programName + " with PID: " + str(os.getpid()))
     collector = Collector("/home/newheres/datacom/802dot11/src/collectorConfig.json")
     collector.initialize()
-    while collector.get_number_of_files() < 5:
+    while not shutdown_flag:
         collector.start()
         collector.write_pcap_in_file()
         print("writing file: " + collector.get_output_file())
         collector.update_output_file()
-        print("updated file to: " + collector.get_output_file())
 
 
 main()
