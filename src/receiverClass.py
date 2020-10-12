98c198e7a0793ccc21537637f8ef1cf2cce30ede
Mauro Furer <maurofurer@gmail.com>
2020-10-12 20:49:31 -0300

    Adding logging functionality in receiverClass

--- src/receiverClass.py
+++ src/receiverClass.py
@@ -17,6 +17,7 @@ import datetime
 import pickle
 import os
 from dataUtilsClass import dataUtils
+import logging
 
 
 class Receiver(dataUtils):
@@ -26,6 +27,7 @@ class Receiver(dataUtils):
             'cfgFromFile': {
                 'host': '',
                 'src_port': 5000,
+                'logFile': '',
                 'outputDir': '',
                 'outputFileMask': '',
                 'outputExt': '',
@@ -64,12 +66,18 @@ class Receiver(dataUtils):
                 except KeyError as e1:
                     print('[' + self.class_name + ']' + ' Exception: ', e1)
                     raise KeyError("Error reading necessary keys to execute the process")
+
+            logging.basicConfig(filename=self.conf['cfgFromFile']['logFile'],
+                                filemode='a',
+                                format='%(asctime)s - %(process)d - %(levelname)s - %(message)s',
+                                level=logging.DEBUG)
             number_of_digits = self.conf['cfgFromFile']['seqDig']
             self.conf['cfgFromProc']['maxSeqNumber'] = 10 ** int(number_of_digits) - 1
             self.conf['cfgFromProc']['seqNumber'] = 0
             self.update_output_file()
             """ A simple echo server """
-            print("[start] esperando por nuevas conexiones")
+            logging.info('watting income connections ...')
+            logging.info('creatting socket server ...')
             # Create a TCP socket
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             # Enable reuse address/port
@@ -78,15 +86,18 @@ class Receiver(dataUtils):
             self.server_address = (self.conf['cfgFromFile']['host'], self.conf['cfgFromFile']['src_port'])
             # print
             # "Starting up echo server on %s port %s" % server_address
+            logging.info('bind socket server ...')
             self.sock.bind(self.server_address)
             # Listen to clients, backlog argument specifies the max no. of queued connections
+            logging.info('listen socket server ...')
             self.sock.listen(self.conf['cfgFromProc']['backlog'])
             print("Waiting to receive message from client")
+            logging.info('accept socket server ...')
             self.client, self.address = self.sock.accept()
 
     def terminate_process(self, signum, frame):
         self.shutdown_flag = True
-        print('(SIGTERM) terminating the process')
+        logging.info('(SIGTERM) terminating the process')
 
     def received_term_sig(self):
         return self.shutdown_flag
@@ -98,45 +109,47 @@ class Receiver(dataUtils):
         while not self.received_term_sig():
             try:
                 container = self.client.recv(self.data_payload)
+                logging.debug('received message')
                 datarcv = pickle.loads(container)
                 if datarcv['type'] == 0:
+                    logging.debug('received OK')
                     self.data = datarcv['payload']
                     self.pkt.append(self.data)
                     i = i + 1
                     self.client.send('OK'.encode())
+                    logging.debug('sent  OK')
                 elif datarcv['type'] == 1:
-                    print("recieved all registries: " + str(i))
+                    logging.debug('received all registries: ' + str(i))
                     file_name = os.path.basename(datarcv['name'])
                     self.conf['cfgFromProc']['outputFile'] = self.conf['cfgFromFile']['outputDir'] + \
                                                              self.conf['cfgFromFile']['outputFileMask'] + \
                                                              file_name.split('_', 1)[1]
-                    print("write outputfile: " + self.conf['cfgFromProc']['outputFile'])
-                    print("File saved")
+                    logging.debug("write outputfile: " + self.conf['cfgFromProc']['outputFile'])
                     self.client.send('OK_CLOSE'.encode())
+                    logging.debug('sent OK_CLOSE')
                     self.write_pcap_in_file()
                     self.pkt = []
                     i = 0
                 else:
                     self.pkt = []
                     self.client.send('OK_CLOSE'.encode())
-                    print("recieved all registries")
+                    logging.debug("received all registries")
                     # cierra la conexion, reicibio el fin de envio
                     # print("Cerrando conexion  ... ")
                     # self.client.close()
                     break
-                    #self.client, self.address = self.sock.accept()
+                    # self.client, self.address = self.sock.accept()
             except Exception as e:
-                print('Exception: process ', e)
+                logging.debug('Exception: process ' + str(e))
+                time.sleep(5)
 
     def send_reg(self):
 
         files = (glob.glob(self.inputDir + self.__inputFileMask + "*." + self.__outputExt)).pop()
-        file=rdpcap(files)
-        print(files)
+        file = rdpcap(files)
+        logging.debug(files)
         for msg in file:
-            print("sending: " + str(len(msg)) + " bytes")
+            logging.debug("sending: " + str(len(msg)) + " bytes")
             self.socket.send(raw(msg))
             data = self.socket.recv(2048)
             print("received: ", data.decode())
-
-
